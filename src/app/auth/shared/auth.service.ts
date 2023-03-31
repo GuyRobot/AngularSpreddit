bc637feb0708894a915df568eedf7de56d07d69f
GuysRGithub <guysdeveloperrobot@gmail.com>
2023-03-31 10:04:54 +0700

    Add refresh token via token interceptor

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -2,7 +2,7 @@ import { SigninResponsePayload } from './../signin/signin.response.payload';
 import { SignupPayload } from './../signup/signup.payload';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { SigninPayload } from '../signin/signin.payload';
 import { LocalStorageService } from 'ngx-webstorage';
 
@@ -36,6 +36,36 @@ export class AuthService {
           this.localStorage.store('expiresAt', res.expiresAt);
           this.localStorage.store('username', res.username);
           return true;
+        })
+      );
+  }
+
+  getJwtToken() {
+    return this.localStorage.retrieve('authenticationToken');
+  }
+
+  getUsername() {
+    return this.localStorage.retrieve('username');
+  }
+
+  refreshToken() {
+    const refreshTokenPayload = {
+      refreshToken: this.refreshToken,
+      username: this.getUsername(),
+    };
+
+    return this.httpClient
+      .post<SigninResponsePayload>(
+        `${this.apiUrl}/refresh`,
+        refreshTokenPayload
+      )
+      .pipe(
+        tap((response) => {
+          this.localStorage.store(
+            'authenticationToken',
+            response['authenticationToken']
+          );
+          this.localStorage.store('expiresAt', response['expiresAt']);
         })
       );
   }
